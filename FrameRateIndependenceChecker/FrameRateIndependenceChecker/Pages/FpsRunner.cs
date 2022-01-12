b04ec8531a28e9aed5dc351a478206580649ec67
John Tringham <john@tringham.co.uk>
2022-01-12 19:07:42 +0000

    erratic plot

--- FrameRateIndependenceChecker/FrameRateIndependenceChecker/Pages/FpsRunner.cs
+++ FrameRateIndependenceChecker/FrameRateIndependenceChecker/Pages/FpsRunner.cs
@@ -10,11 +10,16 @@ namespace FrameRateIndependenceChecker.Pages
         public CompiledSnippet snippet;
         public float fps;
 
-        public FpsRunner(CompiledSnippet snippet, float fps)
+        public FpsRunner(CompiledSnippet snippet, float fps, bool erratic = false)
         {
             this.snippet = snippet;
             this.fps = fps;
-        } 
+            this.erratic = erratic;
+        }
+
+        private Random random = new Random();
+
+        public bool erratic;
 
         public Dictionary<float, float> results = new Dictionary<float, float>();
 
@@ -25,17 +30,27 @@ namespace FrameRateIndependenceChecker.Pages
 
             var increment = 1.0f / fps;
             var time = 0f;
+
+            results.Add(0f, 0f);
+
             try
             {
                 while (time <= duration)
                 {
+                    float inc = increment;
+                    if (erratic)
+                    {
+                        var min = 10f;
+                        var max = 60f;
+                        inc = 1f / (min + (float)random.NextDouble() * (max - min));
+                    }
+
+                    snippet.Run(inc);
                     var value = snippet.GetValue();
+                    
+                    time += inc;
+                    
                     results.Add(time, value);
-
-                    Console.WriteLine($"{time}: {value}");
-                    snippet.Run(increment);
-
-                    time += increment;
                 }
             }
             catch(Exception e)
